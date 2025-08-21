6ef666912ad1ebb5bd181ec8e17d3ff9d1bbf677
EugenioPedraza <a00835064@tec.mx>
2025-08-20 18:04:19 -0600

    second

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,14 +14,11 @@ export default function Home() {
         />
         <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
           <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
+            Buenas tardes Isaac RDZ
             .
           </li>
           <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
+            Aqui podras ver la pagina :) 
           </li>
         </ol>
 
